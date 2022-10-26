1d8a97f72bf7fc6145a4125c2fda78f692dcd334
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2022-10-26 23:51:13 +0900

    2022.10.26

--- sheep on the hill/sheep.js
+++ sheep on the hill/sheep.js
@@ -23,18 +23,40 @@ export class Sheep {
     }
 
     draw(ctx, t, dots){
+        if (!this.time){
+            this.time = t;
+        }
+
+        const now = t - this.time;
+        if (now > this.fpsTime) {
+            this.time = t;
+            this.curFrame += 1;
+            if (this.curFrame == this.totalFrame) {
+                this.curFrame = 0;
+            }
+        }
+        // requestAnimationFrame 함수에서 넘겨받은 타임 스탬프를 시간으로 정의하고
+        // 내가 정한 FPS의 시간과 비교하는 코드
+        // -> 프레임을 증가시키는 속도를 시간에 맞춰서 조절
+
         this.animate(ctx, dots);
     }
 
     animate(ctx, dots){
         // 양의 중심점을 하단 중앙으로 잡아야 언덕 선을 따라간다.
-        this.x = 650;
+        this.x -= this.speed; // 양의 x값을 스테이지 넓이에 양에 넓이를 더한 만큼 초기지정
         this.y = 550;
 
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.fillStyle = '#000000';
-        ctx.fillRect (
+        // drawImage() 함수 : 캔버스에 이미지 그리기
+        ctx.drawImage(
+            this.img,
+            this.imgWidth * this.curFrame,
+            0,
+            this.imgWidth,
+            this.imgHeight,
             -this.sheepWidthHalf,
             -this.sheepHeight + 20,
             this.sheepWidth,
@@ -42,4 +64,18 @@ export class Sheep {
         );
         ctx.restore(); // 캔버스 복귀
     }
+    // 비율에 따른 좌표 찾기 -> 양이 언덕을 따라갈 수 있도록
+    getQuadValue(p0, p1, p2, t) {
+        return (1-t) * (1-t) * p0 + 2 * (1-t) * t * p1 + t * t * p2;
+    }
+
+    getPointOnQuad(x1, y1, x2, y2, x3, y3, t) {
+        return {
+            x: this.getQuadValue(x1, x2, x3, t),
+            y: this.getQuadValue(y1, y2, y3, t),
+        };
+    }
+
 }
+
+
