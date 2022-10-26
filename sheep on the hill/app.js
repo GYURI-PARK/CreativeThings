5d7968ca9d6cea174b9f624cafcc0ad6bce54858
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2022-10-26 14:45:30 +0900

    3:32

--- sheep on the hill/app.js
+++ sheep on the hill/app.js
@@ -1,8 +1,18 @@
+import {
+    Hill
+} from'./hill.js';
+
 class App {
     constructor() {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext("2d");
         document.body.appendChild(this.canvas);
+
+        this.hills = [
+            new Hill('#fd6bea', 0.2, 12),
+            new Hill('#ff59c2', 0.5, 8),
+            new Hill('#ff4674', 1.4, 6)
+        ];
         
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
@@ -18,6 +28,10 @@ class App {
         this.canvas.height = this.stageHeight * 2;
         this.ctx.scale(2,2);
         // canvas 사이즈를 두 배로 해줌으로써 retina display에서도 선명하게 보일 수 있도록 해준다.
+
+        for (let i = 0; i < this.hills.length; i++){
+            this.hills[i].resize(this.stageWidth, this.stageHeight);
+        }
     } // 스크린 사이즈를 가져오기 위한 resize 이벤트
 
     animate(t) {
@@ -25,5 +39,14 @@ class App {
 
         this.ctx.clearRect(0, 0, this.stageWidth, this,stageHeight);
         // canvas 초기화
+
+        let dots;
+        for (let i = 0; i < this.hills.length; i++){
+            dots = this.hills[i].draw(this.ctx);
+        }
     }
 }
+
+window.onload = () => {
+    new App();
+};
--- sheep on the hill/hill.js
+++ sheep on the hill/hill.js
@@ -12,20 +12,64 @@ export class Hill {
         this.stageWidth = stageWidth;
 
         this.points = []; // for 포인트 저장
-        this.gap = Match.ceil(this.stageWidth / (this.total - 2));
+        this.gap = Math.ceil(this.stageWidth / (this.total - 2));
         // stage보다 넓게 언덕을 그려(-2) 양이 자연스럽게 걸어올수있도록 구현 (2:15)
 
         for (let i = 0; i < this.total; i++){
             this.points[i] = {
-                x : i*this.gap,
-                y : this.getY()
+                x: i*this.gap,
+                y: this.getY()
             };
         }
     }
     draw(ctx) {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
         
-    }
+        let cur = this.points[0];
+        let prev = cur;
+
+        let dots = [];
+        // 양의 좌표값 저장
+        ctx.x += this.speed;
+        ctx.moveTo(cur.x, cur.y);
+
+        let prevCx = cur.x;
+        let prevCy = cur.y;
+
+        for (let i = 1; i < this.points.length; i++){
+            cur = this.points[i];
+
+            const cx = (prev.x + cur.x) / 2;
+            const cy = (prev.y + cur.y) / 2;
+            ctx.quadraticCurveTo(prev.x, prev.y, cx, cy);
+            
+            dots.push({
+                x1: prevCx,
+                y1: prevCy,
+                x2: prev.x,
+                y2: prev.y,
+                x3: cx,
+                y3: cy,
+            });
+
+            prev = cur;
+            prevCx = cx;
+            prevCy = cy;
+        }
+
+        ctx.lineTo(prev.x, prev.y);
+        ctx.lineTo(this.stageWidth, this.stageHeight);
+        ctx.lineTo(this.points[0].x, this.stageHeight);
+        ctx.fill();
+
+        return dots; // 양의 좌표를 리턴값으로 넘겨주기
+        
+    } // point배열을 가져와 quadraticCurveTo를 사용해 언덕그리기
     geyY() {
+        const min = this.stageHeight / 8;
+        const max = this.stageHeight - min;
+        return min + Math.random() * max;
 
     } // 언덕의 높낮이를 랜덤으로 주기 위한 함수
 
