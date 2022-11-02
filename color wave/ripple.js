06a015d22104c302c422e67a8d0cdeae8dd5ad72
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2022-11-02 23:50:59 +0900

    3:50

---
+++ color wave/ripple.js
@@ -0,0 +1,23 @@
+
+export class Ripple {
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.radius = 0;
+        this.maxRadius = 0;
+        this.speed = 10;
+    }
+
+    resize(stageWidth, stageHeight) {
+        this.stageWidth = stageWidth;
+        this.stageHeight = stageHeight;
+    }
+
+    start(x,y) {
+
+    }
+
+    animate() {
+        
+    }
+}
