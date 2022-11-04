215c8c5c1314b2b4a955830311d879e588f29dc8
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2022-11-04 12:31:21 +0900

    9:49

--- color wave/app.js
+++ color wave/app.js
@@ -1,4 +1,5 @@
 import {Ripple} from './ripple.js';
+import {Dot} from './dot.js';
 
 class App {
     constructor() {
@@ -6,12 +7,19 @@ class App {
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext('2d');
 
+        this.tmpCanvas = document.createElement('canvas');
+        this.tmpCtx = this.tmpCanvas.getContext('2d');
+
         this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
         this.ripple = new Ripple();
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
+
+        this.radius = 10;
+        this.pixelSize = 30;
+        this.dots = []
 
         this.isLoaded = false;
         this.imgPos = {
@@ -41,6 +49,9 @@ class App {
         this.canvas.width = this.stageWidth * this.pixelRatio;
         this.canvas.height = this.stageHeight * this.pixelRatio;
         this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        this.tmpCanvas.width = this.stageWidth;
+        this.tmpCanvas.height = this.stageHeight;
 
         this.ripple.resize(this.stageWidth, this.stageHeight);
 
@@ -79,6 +90,12 @@ class App {
             this.imgPos.x, this.imgPos.y,
             this.imgPos.width, this.imgPos.height,
         );
+
+        this.drawDots();
+    }
+
+    drawDots() {
+        this.dots = [];
     }
 
     animate() {
